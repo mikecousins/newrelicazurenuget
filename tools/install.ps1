58510cdd73921e64d672b02655112332f08da095
Nick Floyd <nicholas.floyd@gmail.com>
2012-05-06 19:28:18 -0500

    step 2 - adding a startup.cmd file for azure cmd settings

--- tools/install.ps1
+++ tools/install.ps1
@@ -2,3 +2,7 @@ param($installPath, $toolsPath, $package, $project)
 $newrelicMsi = $project.ProjectItems.Item("NewRelicAgent_x64_2.0.8.4.msi")
 $copyToOutput = $newrelicMsi.Properties.Item("CopyToOutputDirectory")
 $copyToOutput.Value = 1
+
+$newrelicMsi = $project.ProjectItems.Item("startup.cmd")
+$copyToOutput = $newrelicMsi.Properties.Item("CopyToOutputDirectory")
+$copyToOutput.Value = 1
